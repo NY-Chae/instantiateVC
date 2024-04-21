83762bc89b4e55b15591e80a23e770b36569e4af
Nayeon Chae <cny1230@naver.com>
2024-04-21 21:49:55 +0900

    not finished yet

--- instantiateVC/instantiateVC/FirstViewController.swift
+++ instantiateVC/instantiateVC/FirstViewController.swift
@@ -4,19 +4,26 @@ import UIKit
 
 class FirstViewController: UIViewController {
     
-    var number = 0
     
-//    required init?(coder: NSCoder) {
-//        super.init(coder: coder)
-//    }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // 여기서 FirstVC 인스턴스화
-        let FirstVC = self.storyboard?.instantiateViewController(withIdentifier: "FirstVC")
-        self.present(FirstVC!, animated: true, completion: nil)
-        
+    var count: Int = 0
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    @IBAction func incrementButton(_ sender: Any) {
+        count += 1
+        countLabel.text = "\(count)"
+    }
+    
+    @IBAction func decrementButton(_ sender: Any) {
+        count -= 1
+        countLabel.text = "\(count)"
+    }
+    
+    @IBAction func nextButton(_ sender: Any) {
+        let secondVC = self.storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        secondVC.receivedCount = self.count
+        self.present(secondVC, animated: true, completion: nil)
     }
 }
 
--- instantiateVC/instantiateVC/SecondViewController.swift
+++ instantiateVC/instantiateVC/SecondViewController.swift
@@ -1,20 +1,16 @@
-//
-//  SecondViewController.swift
-//  instantiateVC
-//
-//  Created by 채나연 on 4/20/24.
-//
+
 
 import UIKit
 
 class SecondViewController: UIViewController {
-
-    var result = 0
+    
+    var receivedCount: Int = 0
+    
+    @IBOutlet weak var countLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        countLabel.text = "\(receivedCount)"
     }
-    
-
 }
+
