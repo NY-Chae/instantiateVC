7a3f03b5edff253741de094b71a6c425e6878ef6
Nayeon Chae <cny1230@naver.com>
2024-04-21 21:39:46 +0900

    not finished yet

--- instantiateVC/instantiateVC/FirstViewController.swift
+++ instantiateVC/instantiateVC/FirstViewController.swift
@@ -1,36 +1,23 @@
-//
-//  ViewController.swift
-//  instantiateVC
-//
-//  Created by 채나연 on 4/20/24.
-//
+
 
 import UIKit
 
 class FirstViewController: UIViewController {
-
-    
-    required init?(coder: NSCoder) {
-        
-    }
     
     var number = 0
     
-    let FirstVC = self().storyboard?.instantiateViewController(withIdentifier: "FirstVC")
-    self.present(firstVC!, animated: true, completion: nill)
-//
-//    
-//    // Storyboard에서 ViewController의 Identifier를 "MyViewController"로 설정했다고 가정
-//    let myViewController = self.storyboard?.instantiateViewController(withIdentifier: "MyViewController")
-//    myViewController?.title = "My View Controller"
-//    self.present(myViewController!, animated: true, completion: nil)
-
+//    required init?(coder: NSCoder) {
+//        super.init(coder: coder)
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 여기서 FirstVC 인스턴스화
+        let FirstVC = self.storyboard?.instantiateViewController(withIdentifier: "FirstVC")
+        self.present(FirstVC!, animated: true, completion: nil)
+        
     }
-
-
 }
+
 
